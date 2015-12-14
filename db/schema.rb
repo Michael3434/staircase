dff3e83c3730901f6146844076d37983df69eea6
Alexis Danan <lxsdanan@gmail.com>
2015-12-14 10:09:18 +0100

    add start/end not here in user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211182645) do
+ActiveRecord::Schema.define(version: 20151214090433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,8 @@ ActiveRecord::Schema.define(version: 20151211182645) do
     t.boolean  "is_renter"
     t.integer  "phone_number"
     t.integer  "apartment_id"
+    t.datetime "startnothere"
+    t.datetime "endnothere"
   end
 
   add_index "users", ["apartment_id"], name: "index_users_on_apartment_id", using: :btree
