ccb657926ceb183b9ceb50a63ead228d87689676
Michael monin <mt.monin@gmail.com>
2016-03-26 22:15:25 +0100

    typoe

--- app/services/sms_sender.rb
+++ app/services/sms_sender.rb
@@ -2,22 +2,21 @@ require 'twilio-ruby'
 
 class SmsSender
 
-  attr_accessor :content, :recipient
-def initialize(content, recipient = [])
-  @content = content # Body
-  @recipient = recipient # Array of string with numbers
-end
+attr_accessor :content, :recipient
+  def initialize(content, recipient = [])
+    @content = content # Body
+    @recipient = recipient # Array of string with numbers
+  end
 
-def send
-  byebug
-  client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTHTOKEN']
-  from = "+33644608116" # Your Twilio number
-    to = @recipient
-    client.account.messages.create(
-      :from => from,
-      :to => to,
-      :body => @content
-    )
-end
+  def send
+    client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTHTOKEN']
+    from = "+33644608116" # Your Twilio number
+      to = @recipient
+      client.account.messages.create(
+        :from => from,
+        :to => to,
+        :body => @content
+      )
+  end
 end
 
